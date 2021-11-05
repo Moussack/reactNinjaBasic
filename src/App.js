1cc58bc03a4d3d30387d1fd2339d99e5f113a7f3
Moussack <reywahyono@gmail.com>
2021-11-05 15:24:58 +0700

    completedd state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [events, setEvents] = useState([
+      { title: 'Mario Birthday', id: 1 },
+      { title: 'How are you?', id: 2 },
+      { title: 'Drinks on me', id: 3 },
+   ]);
+
+   const handleClick = (id) => {
+      console.log(id);
+      setEvents((prevEvents) => {
+         return prevEvents.filter((event) => event.id !== id);
+      });
+   };
+
+   return (
+      <div className="App">
+         {events.map((event) => {
+            return (
+               <div key={event.id}>
+                  <h2>{event.title}</h2>
+                  <button
+                     onClick={() => {
+                        handleClick(event.id);
+                     }}
+                  >
+                     Delete
+                  </button>
+               </div>
+            );
+         })}
+      </div>
+   );
 }
 
 export default App;
+
+/* Array.prototype.Myfilter = function (callback) {
+   const newArray = [];
+   for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+         newArray.push(this[i]);
+      }
+   }
+   return newArray;
+};
+
+Array.prototype.Mymap = function (callback) {
+   const newArray = [];
+   for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this));
+   }
+   return newArray;
+}; */
